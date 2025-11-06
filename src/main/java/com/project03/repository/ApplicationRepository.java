3d6a9a57a0242b9ea214178f05bd079215c4240e
hugo-severino04 <hugo.severino2014@gmail.com>
2025-11-06 12:51:28 -0800

    Imports necessary JPA components.
    just missing imports quick fix

--- src/main/java/com/project03/repository/ApplicationRepository.java
+++ src/main/java/com/project03/repository/ApplicationRepository.java
@@ -2,6 +2,8 @@ package com.project03.repository;
 
 import com.project03.model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
